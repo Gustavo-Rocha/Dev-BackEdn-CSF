78edbd35feebf5cf2f4d4f6681cb5187ba4b0d00
Gustavo Oliveira <Gustavooliveirarocha@hotmail.com>
2023-05-27 19:36:08 -0300

    Implementando Conexao com banco e CRUD

--- WebApi/FilmesApi/FilmesApi/Program.cs
+++ WebApi/FilmesApi/FilmesApi/Program.cs
@@ -1,11 +1,19 @@
+using FilmesApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
+
+builder.Services.AddDbContext<FilmeContext>(opt => opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
